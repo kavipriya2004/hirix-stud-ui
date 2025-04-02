d899769445aec20b791effcbea0217a44bb8252d
kavipriya2004 <kavipriya.kousalya2004@gmail.com>
2025-04-02 16:06:23 +0530

    routing

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,31 +1,54 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "./components/layout/Layout";
 import Home from "./pages/Home";
 import Companies from "./pages/Companies";
 import JobSearch from "./pages/JobSearch";
 import Profile from "./pages/Profile";
 import Notifications from "./pages/Notifications";
 import Settings from "./pages/Settings";
+import SignIn from "./pages/SignIn";
 import Support from "./pages/Support";
-import SignIn from "./pages/SignIn"; 
-import Menu from "./components/Menu";
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        path: 'companies',
+        element: <Companies />
+      },
+      {
+        path: 'job-search',
+        element: <JobSearch />
+      },
+      {
+        path: 'profile',
+        element: <Profile />
+      },
+      {
+        path: 'notifications',
+        element: <Notifications />
+      },
+      {
+        path: 'settings',
+        element: <Settings />
+      },
+      {
+        path: 'support',
+        element: <Support />
+      }
+    ]
+  },
+  {
+    path: '/signin',
+    element: <SignIn />
+  }
+]);
 
-function AppRoutes() {
-  return (
-    <Router>
-      <Menu />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/companies" element={<Companies />} />
-        <Route path="/job-search" element={<JobSearch />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/notifications" element={<Notifications />} />
-        <Route path="/settings" element={<Settings />} />
-        <Route path="/support" element={<Support />} />
-         <Route path="/signin" element={<SignIn />} />
-      </Routes>
-    </Router>
-  );
-}
-
-export default AppRoutes;
+export default router;
