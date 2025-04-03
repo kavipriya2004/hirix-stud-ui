88d38d2218f7834f71aa8dfd54472a2603106925
kavipriya2004 <kavipriya.kousalya2004@gmail.com>
2025-04-03 14:08:06 +0530

    layout fixed

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import Menu from '../Menu';
+import Menu from '../menu/Menu';
 import Navbar from '../navbar/Navbar';
 import { Outlet } from 'react-router-dom';
-import "../../App.css"; 
+import './Layout.css';
 
 const Layout = () => {
   return (
-    <div className="app-container">
-      {/* Left sidebar with menu */}
-      <div className="sidebar">
-        <div className="logo-container">
-          <h1 className="logo">Hirix</h1>
-        </div>
+    <div className="layout-container">
+      {/* Left Sidebar (Menu) */}
+      <aside className="sidebar">
         <Menu />
-      </div>
-      
-       {/* Main content area */}
-       <div className="main-content">
-        {/* Navbar Component */}
+      </aside>
+
+      {/* Main Content Area */}
+      <div className="main-content">
+        {/* Navbar at the top */}
         <Navbar />
         
-        {/* Page content will be rendered here */}
+        {/* Page Content */}
         <div className="page-content">
           <Outlet />
         </div>
