efd7e0e6409ccb224c67f93b588e94f3deef552e
kavipriya2004 <kavipriya.kousalya2004@gmail.com>
2025-04-17 12:02:30 +0530

    routes updated

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,7 +3,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import Home from "./pages/Home";
 import Companies from "./pages/companies/Companies";
-import JobSearch from "./pages/JobSearch";
+import JobSearch from "./pages/jobsearch/JobSearch";
 import Profile from "./pages/Profile";
 import Notifications from "./pages/Notifications";
 import Settings from "./pages/Settings";
